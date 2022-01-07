e7fde42962c27d1efc7691388b1ecca2abe466aa
Zac Allen <zac.allen@adyen.com>
2022-01-07 18:45:17 +0000

    recurring payment initially working, version controll and type to be fixed

--- src/recurring/recurring.js
+++ src/recurring/recurring.js
@@ -32,37 +32,67 @@ function makeRecurringCall() {
 					// Reference for now
 					console.log(b)
 
-					var savedPm = ''
+					var savedPm = null
 
 					if (b['bank']) {
-						savedPm = `
-									<strong>Bank:</strong>
-										<p class='pmDetailsP'>Owner: ${b['bank']['ownerName']}</p>
-										<p class='pmDetailsP'>Bank Name: ${b['bank']['bankName']}</p>
-										<p class='pmDetailsP'>BIC: ${b['bank']['bic']}</p>
-										<p class='pmDetailsP'>Country: ${b['bank']['countryCode']}</p>
-										<p class='pmDetailsP'>IBAN: ${b['bank']['iban']}</p>
 
-
-
-								  `
+						savedPm = `<strong>Bank:</strong>
+									<p class='pmDetailsP'>Owner: ${b['bank']['ownerName']}</p>
+									<p class='pmDetailsP'>Bank Name: ${b['bank']['bankName']}</p>
+									<p class='pmDetailsP'>BIC: ${b['bank']['bic']}</p>
+									<p class='pmDetailsP'>Country: ${b['bank']['countryCode']}</p>
+									<p class='pmDetailsP'>IBAN: ${b['bank']['iban']}</p>`
+					
 					} else {
-						savedPm = `
-									<strong>Card: </strong>
-										<p class='pmDetailsP'>Holder Name: ${b['card']['holderName']}</p>
-										<p class='pmDetailsP'>Card Bin: ${b['additionalData']['cardBin']}</p>
-										<p class='pmDetailsP'>Last 4 Digits: ${b['card']['number']}</p>
-										<p class='pmDetailsP'>Expiry Date: ${b['card']['expiryMonth']}/${b['card']['expiryYear']}</p>
-								  `
+
+						savedPm = `<strong>Card: </strong>
+									<p class='pmDetailsP'>Holder Name: ${b['card']['holderName']}</p>
+									<p class='pmDetailsP'>Card Bin: ${b['additionalData']['cardBin']}</p>
+									<p class='pmDetailsP'>Last 4 Digits: ${b['card']['number']}</p>
+									<p class='pmDetailsP'>Expiry Date: ${b['card']['expiryMonth']}/${b['card']['expiryYear']}</p>`
+					
 					}
 
-					a.innerHTML = `
-									<p><strong>Variant: </strong>${b['variant']}</p>
-									<p> ${savedPm} </p>
+					a.innerHTML = `<p><strong>Variant: </strong>${b['variant']}</p>
 									<p><strong>Recurring Detail Reference: </strong>${b['recurringDetailReference']}</p>
-								  `
+									<p><strong>Creation Date: </strong> ${b['creationDate']}</p>
+									${savedPm}
+									<p><button class="makeRecPayment" id="pay_${b['recurringDetailReference']}" value="${b['recurringDetailReference']}">Make a payment</button> <button class="diasbleRecRef" id="disable_${b['recurringDetailReference']}">Disable</button></p>`
+				
 				}
+
+			var makeRecPayment = document.getElementsByClassName('makeRecPayment');
+
+			for (var i = 0; i < makeRecPayment.length; i++) {
+
+				let y = makeRecPayment[i];
+				y.addEventListener('click', function () {
+					
+					let rPaymentData = {
+						   amount:{
+						      value: 2000,
+						      currency: "EUR"
+						   },
+						   paymentMethod:{
+						      type:"sepadirectdebit",
+						      storedPaymentMethodId: y.value
+						   },
+						   reference: "YOUR_ORDER_NUMBER",
+						   shopperInteraction: "ContAuth",
+						   recurringProcessingModel: "Subscription",
+						   shopperReference:shopperReference
+						}
+
+					makePayment(rPaymentData)
+						.then(res => {
+							console.log(res)
+						})
+				})
+
+			}
+			
 		})
+
 };
 
 
---
+++ src/recurring/recurringParams.js
@@ -0,0 +1,25 @@
+const paymentsDefaultConfig = {
+    // shopperReference: shopperReference,
+    reference: "reference",
+    countryCode: "countryCode",
+    shopperLocale: "shopperLocale",
+    shopperName: 'John Doe',
+    shopperEmail: 's.hopper@adyen.com',
+    channel: 'Web',
+    amount: {
+        value: 100,
+        currency: "EUR"
+    },
+    lineItems: [
+        {
+            id: '1',
+            description: 'Test Item 1',
+            amountExcludingTax: 10000,
+            amountIncludingTax: 11800,
+            taxAmount: 1800,
+            taxPercentage: 1800,
+            quantity: 1,
+            taxCategory: 'High'
+        }
+    ]
+};
