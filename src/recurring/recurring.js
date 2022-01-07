c3c758dc093e321878dd7b5890586c666fe3113c
Zac Allen <zac.allen@adyen.com>
2022-01-07 13:03:17 +0000

    recurring references displaying properly for banks and cards

--- src/recurring/recurring.js
+++ src/recurring/recurring.js
@@ -18,27 +18,51 @@ function makeRecurringCall() {
 	listRecurringDetails(rData)
 		.then(res => {
 			updateResponseContainer(res);	
-			// recurringDetails.innerText = JSON.stringify(res, null, 1);
+			// recurringDetails.innerHTML = `<pre>${JSON.stringify(res, null, 1)}</pre>`;
 			const details = res['details']
-			console.log(details)
+			// console.log(details)
 
 				for (var i = 0; i < details.length; i++) {
 					
 					const a = document.createElement('div')
 					a.classList.add('shopperDetails')
-
-					for(let [key, value] of Object.entries(details[i])) {
-						console.log(key)
-						console.log(value)
-					}
-					a.innerText = JSON.stringify(details[i]['RecurringDetail'], null, 1)
 					recurringDetails.appendChild(a)
-
-					console.log(details[i]['RecurringDetail']['recurringDetailReference'])
+					var b = details[i]['RecurringDetail']
 					
+					// Reference for now
+					console.log(b)
+
+					var savedPm = ''
+
+					if (b['bank']) {
+						savedPm = `
+									<strong>Bank:</strong>
+										<p class='pmDetailsP'>Owner: ${b['bank']['ownerName']}</p>
+										<p class='pmDetailsP'>Bank Name: ${b['bank']['bankName']}</p>
+										<p class='pmDetailsP'>BIC: ${b['bank']['bic']}</p>
+										<p class='pmDetailsP'>Country: ${b['bank']['countryCode']}</p>
+										<p class='pmDetailsP'>IBAN: ${b['bank']['iban']}</p>
+
+
+
+								  `
+					} else {
+						savedPm = `
+									<strong>Card: </strong>
+										<p class='pmDetailsP'>Holder Name: ${b['card']['holderName']}</p>
+										<p class='pmDetailsP'>Card Bin: ${b['additionalData']['cardBin']}</p>
+										<p class='pmDetailsP'>Last 4 Digits: ${b['card']['number']}</p>
+										<p class='pmDetailsP'>Expiry Date: ${b['card']['expiryMonth']}/${b['card']['expiryYear']}</p>
+								  `
+					}
+
+					a.innerHTML = `
+									<p><strong>Variant: </strong>${b['variant']}</p>
+									<p> ${savedPm} </p>
+									<p><strong>Recurring Detail Reference: </strong>${b['recurringDetailReference']}</p>
+								  `
 				}
 		})
-
 };
 
 
