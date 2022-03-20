2942614e673c2242321a30fa6620525904d0aa8b
Zac Allen <zac.allen@adyen.com>
2022-03-20 14:12:48 +0100

    removed edit feature temporarily, tidied up css and layour

--- src/paymentLinks/paymentLink.js
+++ src/paymentLinks/paymentLink.js
@@ -17,15 +17,16 @@ const pblFormParams = {
 };
 
 const paymentLinkForm = document.getElementById('paymentLinkForm');
+const paymentLinkTable = document.getElementById('paymentLinkTable');
 
 (function createPblForm() {
 
 	for (let [key, value] of Object.entries(pblFormParams)) {
 		
-		let a = document.createElement("div");
+		let a = document.createElement("tr");
 		a.classList.add("pl-inputs");
-		paymentLinkForm.append(a);
-		a.innerHTML = `<label type='text'>${key}</label><input id='pl-${key}' type='text' name=${key} value=${value}>`;
+		paymentLinkTable.append(a);
+		a.innerHTML = `<td type='text'>${key}</td> <td><input id='pl-${key}' type='text' name=${key} value=${value}></td>`;
 
 	}
 
@@ -37,22 +38,22 @@ var extraFieldId = 0;
 
 function addDataToRequest() {
 	
-	let newField = document.createElement('div')
-	newField.classList.add('pl-inputs')
+	let newField = document.createElement('tr');
+	newField.classList.add('pl-inputs');
 
 	let buttonId = `extraField_${extraFieldId}`;
 	let paramKey = `paramKey_${extraFieldId}`;
 	let valueKey = `valueKey_${extraFieldId}`;
 	extraFieldId += 1;
 
-	newField.innerHTML = `<input id=${paramKey} type='text' placeholder='Parameter'> <input id=${valueKey} type='text' placeholder='Value'> <button id=${buttonId} class='addField'>Add field</button>`
-	paymentLinkForm.append(newField)
+	newField.innerHTML = `<td><input id=${paramKey} type='text' placeholder='Parameter'></td> <td><input id=${valueKey} type='text' placeholder='Value'></td> <td><button id=${buttonId} class='addField'>Add field</button></td>`;
+	paymentLinkTable.append(newField);
 
-	var addFieldButton = document.getElementsByClassName('addField')
+	var addFieldButton = document.getElementsByClassName('addField');
 
 	for (var i = 0; i < addFieldButton.length; i++) {
 		
-		let aFBtn= addFieldButton[i]
+		let aFBtn= addFieldButton[i];
 
 		aFBtn.addEventListener('click', () => {
 			
@@ -62,65 +63,48 @@ function addDataToRequest() {
 
 			if (addedParam !== '') {
 
-				newField.innerHTML = `<span id="paramField_${keyId[1]}">${addedParam}</span> <span id="valueField_${keyId[1]}">${addedValue}</span> <button id="removeField_${keyId[1]}" class='removeField'>Delete Field</button> <button id="editField_${keyId[1]}" class='editField'>Edit Field</button>`;
-				newField.id = `newField_${keyId[1]}`
+				newField.innerHTML = `<td><span id="paramField_${keyId[1]}" value='${addedParam}'>${addedParam}</span></td> <td><span id="valueField_${keyId[1]}" value="${addedValue}">${addedValue}</span></td> <td><button id="removeField_${keyId[1]}" class='removeField'>Delete Field</button></td>`;
+				newField.id = `newField_${keyId[1]}`;
 				additionalData[`${addedParam}`] = addedValue;
 
 				// Calls functions to process the querySelector
-				deleteField.call()
-				editField.call()
+				deleteField.call();
 				
 			} else {
-				
+
 				document.getElementById(paramKey).className = 'formError';
 				alert(`Parameter needed! Go to https://docs.adyen.com/api-explorer/#/CheckoutService/v68/post/paymentLinks to see available parameters`);
 
-			}
-
+			};
 		});
 	};
 };
 
 
 function deleteField() {
+
 	var removeFieldButton = document.querySelectorAll('.removeField');
 	
 	for (var i = 0; i < removeFieldButton.length; i++) {
-		let rFBtn = removeFieldButton[i]
+		let rFBtn = removeFieldButton[i];
 		rFBtn.addEventListener('click', () => {
-			console.log("event")
 			let keyId = rFBtn.id.split('_');
-			let param = document.getElementById(`paramField_${keyId[1]}`)
-			let field = document.getElementById(`newField_${keyId[1]}`)
+			let param = document.getElementById(`paramField_${keyId[1]}`);
+			let field = document.getElementById(`newField_${keyId[1]}`);
 			try {
 				delete additionalData[`${param.innerText}`];
-				field.remove()
+				field.remove();
 			} catch (error) {
-				console.log(error)
-			}
-		})
-	}
-}
-
-
-function editField() {
-
-    var editFieldButton = document.getElementsByClassName('editField');
-
-    for (var i = 0; i < editFieldButton.length; i++) {
-        let eFBtn = editFieldButton[i]
-        eFBtn.addEventListener('click', () => {
-        	console.log("event")
-        })
-    }
-}
+				console.log(error);
+			};
+		});
+	};
+};
 
 const addDataButton = document.getElementById('addDataButton');
-
 addDataButton.addEventListener('click', () => {	addDataToRequest.call() });
 
 const createLink = document.getElementById('createPaymentLink');
-
 createLink.addEventListener('click', function() {
 
 	var standardData = {
@@ -140,7 +124,7 @@ createLink.addEventListener('click', function() {
 
 	var paymentLinkRequest = { ...standardData, ...additionalData };
 
-	const a = document.querySelector('.paymentLinkData')
+	const a = document.querySelector('.paymentLinkData');
 
 		createPaymentLink(paymentLinkRequest)
 			.then(response => {
